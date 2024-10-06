0dc1bace3a5a9e2dce8dbcf2895641d42dafa4a9
Iryna Yanusheuskaya <irinaminsk@gmail.com>
2024-10-06 18:40:27 +0200

    Use Cmake to build project

--- multy/two_threads_task.cpp
+++ multy/two_threads_task.cpp
@@ -5,8 +5,12 @@
 #include <condition_variable>
 #include <vector>
 #include <filesystem>
+#include <boost/program_options.hpp>
+#include <iostream>
+#include <string>
 
 namespace fs = std::filesystem;
+namespace po = boost::program_options;
 namespace
 {
 	// Buffer size for reading chunks of the file
@@ -18,21 +22,20 @@ namespace
 	bool doneReading = false;
 	bool errorReading = false;
 	bool errorWriting = false;
-	
+
 	// Mutex and condition variable to synchronize the threads
 	std::mutex mtx;
 	std::condition_variable cv;
 }
 
-
-void readFromFile(const std::string& sourceFilename)
+void readFromFile(const std::string &sourceFilename)
 {
 	std::fstream sourceFile(sourceFilename, std::ios::binary | std::ios::in);
 	if (!sourceFile)
 	{
 		std::cerr << "\nError: Could not open source file " << sourceFilename << std::endl;
 		errorReading = true;
-		cv.notify_one();  // Notify writer thread that reading is crashed
+		cv.notify_one(); // Notify writer thread that reading is crashed
 		return;
 	}
 
@@ -41,31 +44,31 @@ void readFromFile(const std::string& sourceFilename)
 		std::vector<char> tempBuffer(BUFFER_SIZE);
 
 		sourceFile.read(tempBuffer.data(), BUFFER_SIZE);
-		std::streamsize bytesRead = sourceFile.gcount();  // Get actual bytes read
-		
+		std::streamsize bytesRead = sourceFile.gcount(); // Get actual bytes read
+
 		if (bytesRead > 0)
 		{
 			std::unique_lock<std::mutex> lock(mtx);
 			buffer.assign(tempBuffer.begin(), tempBuffer.begin() + bytesRead);
 			dataAvailable = true;
 			cv.notify_one();
-			cv.wait(lock, [] { return errorWriting || !dataAvailable; });
+			cv.wait(lock, []
+					{ return errorWriting || !dataAvailable; });
 		}
 	}
 	if (!errorWriting)
 	{
 		std::unique_lock<std::mutex> lock(mtx);
 		doneReading = true;
-		cv.notify_one();  // Notify writer thread that reading is done
+		cv.notify_one(); // Notify writer thread that reading is done
 	}
 }
 
-
-void writeToFile(const std::string& targetFilename)
+void writeToFile(const std::string &targetFilename)
 {
 	if (fs::exists(targetFilename))
 	{
-		std::cerr << "\nError: Target file " << targetFilename << " is exist."<<std::endl;
+		std::cerr << "\nError: Target file " << targetFilename << " is exist." << std::endl;
 		errorWriting = true;
 		cv.notify_one();
 		return;
@@ -79,47 +82,67 @@ void writeToFile(const std::string& targetFilename)
 		return;
 	}
 
-	while (true) {
+	while (true)
+	{
 		std::unique_lock<std::mutex> lock(mtx);
-		cv.wait(lock, [] { return errorReading || dataAvailable || doneReading; });
+		cv.wait(lock, []
+				{ return errorReading || dataAvailable || doneReading; });
 
 		if (dataAvailable)
 		{
 			targetFile.write(buffer.data(), buffer.size());
 			dataAvailable = false;
-			cv.notify_one();  // Notify reader thread to continue reading
+			cv.notify_one(); // Notify reader thread to continue reading
 		}
 		else
 		{
 			if (errorReading || doneReading)
 			{
-				break;  // Break when reading is done or can't open source file
+				break; // Break when reading is done or can't open source file
 			}
 		}
 	}
 }
 
-int main(void)
+
+
+int main(int argc, char *argv[])
 {
-	std::string sourceFilename;
-	std::cout << "Enter souece file path: ";
-	std::cin >> sourceFilename;
-
-	std::string targetFilename;
-	std::cout << "Enter target file path: ";
-	std::cin >> targetFilename;
-
-
-	// Start the reader and writer threads
-	std::thread readerThread(readFromFile, sourceFilename);
-	std::thread writerThread(writeToFile, targetFilename);
-
-	// Wait for both threads to finish
-	readerThread.join();
-	writerThread.join();
-	if (doneReading)
+	try
 	{
-		std::cout << "File copying completed successfully." << std::endl;
+		po::options_description desc{"Allowed options"};
+		desc.add_options()("help,h", "Show help message")("name,n", po::value<std::string>(), "Enter your name");
+
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm);
+
+		if (vm.count("help"))
+		{
+			std::cout << desc << "\n";
+		}
+
+		if (vm.count("name"))
+		{
+			std::cout << "Hello, " << vm["name"].as<std::string>() << "!\n";
+		}
+
+		// Start the reader and writer threads
+		// std::thread readerThread(readFromFile, sourceFilename);
+		// std::thread writerThread(writeToFile, targetFilename);
+
+		// // Wait for both threads to finish
+		// readerThread.join();
+		// writerThread.join();
+		// if (doneReading)
+		// {
+		// 	std::cout << "File copying completed successfully." << std::endl;
+		// }
 	}
+	catch (const po::error &ex)
+	{
+		std::cerr << ex.what() << "\n";
+	}
+
 	return 0;
 }
